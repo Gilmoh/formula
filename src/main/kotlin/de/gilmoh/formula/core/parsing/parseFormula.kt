2edda95fb11c1402a59be278b8a5d42511bf0939
TimothyGillespie <timothy@gillespie.eu>
2021-08-14 14:35:12 +0200

    Template parseFormula with dummy value

---
+++ src/main/kotlin/de/gilmoh/formula/core/parsing/parseFormula.kt
@@ -0,0 +1,8 @@
+package de.gilmoh.formula.core.parsing
+
+import de.gilmoh.formula.core.models.Variable
+import de.gilmoh.formula.core.models.interfaces.Formula
+
+fun parseFormula(input: String): Formula {
+    return Variable("x")
+}
